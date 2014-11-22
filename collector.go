deda97073d16248176c649b040c6feabe2866d43
Quinn Slack <qslack@qslack.com>
2014-11-22 01:05:11 -0800

    remote and chunked collectors

--- collector.go
+++ collector.go
@@ -1,5 +1,19 @@
 package apptrace
 
+import (
+	"bufio"
+	"crypto/tls"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"sync"
+	"time"
+)
+
 // A Collector collects events that occur in spans.
 type Collector interface {
 	Collect(SpanID, ...Annotation) error
@@ -9,4 +23,333 @@ type Collector interface {
 // Store.
 func NewLocalCollector(s Store) Collector {
 	return s
+}
+
+// A ChunkedCollector groups annotations together that have the same
+// span and calls its underlying collector's Collect method with the
+// chunked data periodically (instead of immediately).
+type ChunkedCollector struct {
+	// Collector is the underlying collector that spans are sent to.
+	Collector
+
+	// MinInterval is the minimum time period between calls to the
+	// underlying collector's Collect method.
+	MinInterval time.Duration
+
+	// The last error from the underlying Collector's Collect method,
+	// if any. It will be returned to the next caller of Collect and
+	// this field will be set to nil.
+	lastErr error
+
+	started, stopped bool
+	stopChan         chan struct{}
+
+	pending         []SpanID
+	pendingBySpanID map[SpanID]*collectPacket
+
+	// mu protects pending, pendingBySpanID, lastErr, started,
+	// stopped, and stopChan.
+	mu sync.Mutex
+}
+
+// Collect adds the span and annotations to a local buffer until the
+// next call to Flush (or when MinInterval elapses), at which point
+// they are sent (grouped by span) to the underlying collector.
+func (cc *ChunkedCollector) Collect(span SpanID, anns ...Annotation) error {
+	cc.mu.Lock()
+	defer cc.mu.Unlock()
+
+	if cc.stopped {
+		return errors.New("ChunkedCollector is stopped")
+	}
+	if !cc.started {
+		cc.start()
+	}
+
+	if cc.pendingBySpanID == nil {
+		cc.pendingBySpanID = map[SpanID]*collectPacket{}
+	}
+
+	if p, present := cc.pendingBySpanID[span]; present {
+		if len(anns) > 0 {
+			p.Annotations = append(p.Annotations, anns...)
+		}
+	} else {
+		cc.pendingBySpanID[span] = &collectPacket{span, anns}
+		cc.pending = append(cc.pending, span)
+	}
+
+	if err := cc.lastErr; err != nil {
+		cc.lastErr = nil
+		return err
+	}
+	return nil
+}
+
+// Flush immediately sends all pending spans to the underlying
+// collector.
+func (cc *ChunkedCollector) Flush() error {
+	cc.mu.Lock()
+	pendingBySpanID := cc.pendingBySpanID
+	pending := cc.pending
+	cc.pendingBySpanID = nil
+	cc.pending = nil
+	cc.mu.Unlock()
+
+	var errs []error
+	for _, spanID := range pending {
+		p := pendingBySpanID[spanID]
+		if err := cc.Collector.Collect(p.SpanID, p.Annotations...); err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	if len(errs) == 1 {
+		return errs[0]
+	} else if len(errs) > 1 {
+		return fmt.Errorf("ChunkedCollector: multiple errors: %v", errs)
+	}
+	return nil
+}
+
+func (cc *ChunkedCollector) start() {
+	cc.stopChan = make(chan struct{})
+	cc.started = true
+	go func() {
+		for {
+			t := time.After(cc.MinInterval)
+			select {
+			case <-t:
+				if err := cc.Flush(); err != nil {
+					cc.mu.Lock()
+					cc.lastErr = err
+					cc.mu.Unlock()
+				}
+			case <-cc.stopChan:
+				return // stop
+			}
+		}
+	}()
+}
+
+// Stop stops the collector. After stopping, no more data will be sent
+// to the underlying collector and calls to Collect will fail.
+func (cc *ChunkedCollector) Stop() {
+	cc.mu.Lock()
+	defer cc.mu.Unlock()
+	close(cc.stopChan)
+	cc.stopped = true
+}
+
+// A collectPacket is sent between a client and server collector.
+type collectPacket struct {
+	SpanID      SpanID
+	Annotations Annotations
+}
+
+// NewRemoteCollector creates a collector that sends data to a
+// collector server (created with NewServer). It sends data
+// immediately when Collect is called. To send data in chunks, use a
+// ChunkedCollector.
+func NewRemoteCollector(addr string) *RemoteCollector {
+	return &RemoteCollector{
+		addr: addr,
+		dial: func() (net.Conn, error) {
+			return net.Dial("tcp", addr)
+		},
+	}
+}
+
+// NewTLSRemoteCollector creates a RemoteCollector that uses TLS.
+func NewTLSRemoteCollector(addr string, tlsConfig *tls.Config) *RemoteCollector {
+	return &RemoteCollector{
+		addr: addr,
+		dial: func() (net.Conn, error) {
+			return tls.Dial("tcp", addr, tlsConfig)
+		},
+	}
+}
+
+// A RemoteCollector sends data to a collector server (created with
+// NewServer).
+type RemoteCollector struct {
+	addr string
+
+	dial func() (net.Conn, error)
+
+	mu   sync.Mutex // guards conn
+	conn net.Conn
+
+	// Log is the logger to use for errors and warnings. If nil, a new
+	// logger is created.
+	Log   *log.Logger
+	logMu sync.Mutex
+
+	// Debug is whether to log debug messages.
+	Debug bool
+}
+
+func (rc *RemoteCollector) Collect(span SpanID, anns ...Annotation) error {
+	return rc.collectAndRetry(collectPacket{span, anns})
+}
+
+// connect makes a connection to the collector server. It must be
+// called with rc.mu held.
+func (rc *RemoteCollector) connect() error {
+	if rc.conn != nil {
+		rc.conn.Close()
+		rc.conn = nil
+	}
+
+	c, err := rc.dial()
+	if err == nil {
+		rc.conn = c
+	}
+	return err
+}
+
+// Close closes the connection to the server.
+func (rc *RemoteCollector) Close() error {
+	rc.mu.Lock()
+	defer rc.mu.Unlock()
+
+	if rc.conn != nil {
+		err := rc.conn.Close()
+		rc.conn = nil
+		return err
+	}
+	return nil
+}
+
+func (rc *RemoteCollector) collectAndRetry(p collectPacket) error {
+	rc.mu.Lock()
+	defer rc.mu.Unlock()
+
+	if rc.conn != nil {
+		if err := rc.collect(p); err == nil {
+			return nil
+		}
+		if rc.Debug {
+			rc.log().Printf("Reconnecting to send %v", p.SpanID)
+		}
+	}
+	if err := rc.connect(); err != nil {
+		return err
+	}
+	return rc.collect(p)
+}
+
+func (rc *RemoteCollector) collect(p collectPacket) error {
+	bw := bufio.NewWriter(rc.conn)
+	enc := json.NewEncoder(bw)
+	if rc.Debug {
+		rc.log().Printf("Sending %v", p.SpanID)
+	}
+	if err := enc.Encode(p); err != nil {
+		return err
+	}
+	if err := bw.Flush(); err != nil {
+		return err
+	}
+	if rc.Debug {
+		rc.log().Printf("Sent %v", p.SpanID)
+	}
+	return nil
+}
+
+func (rc *RemoteCollector) log() *log.Logger {
+	rc.logMu.Lock()
+	defer rc.logMu.Unlock()
+	if rc.Log == nil {
+		rc.Log = log.New(os.Stderr, fmt.Sprintf("RemoteCollector[%s]: ", rc.addr), log.LstdFlags|log.Lmicroseconds)
+	}
+	return rc.Log
+}
+
+// NewServer creates and starts a new server that listens for
+// spans and annotations on l and adds them to the collector c.
+//
+// Call the CollectorServer's Start method to start listening and
+// serving.
+func NewServer(l net.Listener, c Collector) *CollectorServer {
+	cs := &CollectorServer{c: c, l: l}
+	return cs
+}
+
+// A CollectorServer listens for spans and annotations and adds them
+// to a local collector.
+type CollectorServer struct {
+	c Collector
+	l net.Listener
+
+	// Log is the logger to use for errors and warnings. If nil, a new
+	// logger is created.
+	Log   *log.Logger
+	logMu sync.Mutex
+
+	// Debug is whether to log debug messages.
+	Debug bool
+
+	// Trace is whether to log all data that is received.
+	Trace bool
+}
+
+// Start starts the server.
+func (cs *CollectorServer) Start() {
+	for {
+		conn, err := cs.l.Accept()
+		if err != nil {
+			cs.log().Printf("Accept: %s", err)
+			continue
+		}
+
+		if cs.Debug {
+			cs.log().Printf("Client %s connected", conn.RemoteAddr())
+		}
+
+		go cs.handleConn(conn)
+	}
+}
+
+func (cs *CollectorServer) handleConn(conn net.Conn) (err error) {
+	defer func() {
+		if err != nil {
+			cs.log().Printf("Client %s: %s", conn.RemoteAddr(), err)
+		}
+	}()
+	defer conn.Close()
+
+	br := bufio.NewReader(conn)
+	dec := json.NewDecoder(br)
+	for {
+		var p collectPacket
+		if err := dec.Decode(&p); err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return fmt.Errorf("Decode: %s", err)
+		}
+
+		if cs.Debug || cs.Trace {
+			cs.log().Printf("Client %s: received span %v with %d annotations", conn.RemoteAddr(), p.SpanID, len(p.Annotations))
+		}
+		if cs.Trace {
+			for i, ann := range p.Annotations {
+				cs.log().Printf("Client %s: span %v: annotation %d: %s=%q", conn.RemoteAddr(), p.SpanID.Span, i, ann.Key, ann.Value)
+			}
+		}
+
+		if err := cs.c.Collect(p.SpanID, p.Annotations...); err != nil {
+			return fmt.Errorf("Collect %v: %s", p.SpanID, err)
+		}
+	}
+}
+
+func (cs *CollectorServer) log() *log.Logger {
+	cs.logMu.Lock()
+	defer cs.logMu.Unlock()
+	if cs.Log == nil {
+		cs.Log = log.New(os.Stderr, fmt.Sprintf("CollectorServer[%s]: ", cs.l.Addr()), log.LstdFlags|log.Lmicroseconds)
+	}
+	return cs.Log
 }
--- collector_test.go
+++ collector_test.go
@@ -1,5 +1,200 @@
 package apptrace
 
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"fmt"
+	"net"
+	"reflect"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestCollectorServer(t *testing.T) {
+	var (
+		packets   []collectPacket
+		packetsMu sync.Mutex
+	)
+	mc := &mockCollector{
+		Collect_: func(span SpanID, anns ...Annotation) error {
+			packetsMu.Lock()
+			defer packetsMu.Unlock()
+			packets = append(packets, collectPacket{span, anns})
+			return nil
+		},
+	}
+
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("collector listening on %s", l.Addr())
+
+	cs := NewServer(l, mc)
+	go cs.Start()
+
+	cc := &collectorT{t, NewRemoteCollector(l.Addr().String())}
+
+	collectPackets := []collectPacket{
+		{SpanID{1, 2, 3}, Annotations{{"k1", []byte("v1")}}},
+		{SpanID{2, 3, 4}, Annotations{{"k2", []byte("v2")}}},
+	}
+	for _, p := range collectPackets {
+		cc.MustCollect(p.SpanID, p.Annotations...)
+	}
+	if err := cc.Collector.(*RemoteCollector).Close(); err != nil {
+		t.Error(err)
+	}
+
+	time.Sleep(20 * time.Millisecond)
+	if !reflect.DeepEqual(packets, collectPackets) {
+		t.Errorf("server collected %v, want %v", packets, collectPackets)
+	}
+}
+
+func TestCollectorServer_stress(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	const (
+		n             = 1000
+		allowFailures = 0
+		errorEvery    = 1000000
+	)
+
+	var (
+		packets   = map[SpanID]struct{}{}
+		packetsMu sync.Mutex
+	)
+	mc := &mockCollector{
+		Collect_: func(span SpanID, anns ...Annotation) error {
+			packetsMu.Lock()
+			defer packetsMu.Unlock()
+			packets[span] = struct{}{}
+			// log.Printf("Added %v", span)
+
+			// Occasional errors, which should cause the client to
+			// reconnect.
+			if len(packets)%(errorEvery+1) == 0 {
+				return errors.New("fake error")
+			}
+			return nil
+		},
+	}
+
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cs := NewServer(l, mc)
+	go cs.Start()
+
+	cc := &collectorT{t, NewRemoteCollector(l.Addr().String())}
+	// cc.Collector.(*RemoteCollector).Debug = true
+
+	want := make(map[SpanID]struct{}, n)
+	for i := 0; i < n; i++ {
+		id := NewRootSpanID()
+		want[id] = struct{}{}
+		cc.MustCollect(id)
+	}
+	if err := cc.Collector.(*RemoteCollector).Close(); err != nil {
+		t.Error(err)
+	}
+
+	time.Sleep(20 * time.Millisecond)
+	var missing []string
+	for spanID := range want {
+		if _, present := packets[spanID]; !present {
+			missing = append(missing, fmt.Sprintf("span %v was not collected", spanID))
+		}
+	}
+	if len(missing) > allowFailures {
+		for _, missing := range missing {
+			t.Error(missing)
+		}
+	}
+}
+
+func TestTLSCollectorServer(t *testing.T) {
+	var numPackets int
+	mc := &mockCollector{
+		Collect_: func(span SpanID, anns ...Annotation) error {
+			numPackets++
+			return nil
+		},
+	}
+
+	l, err := tls.Listen("tcp", "127.0.0.1:0", &localhostTLSConfig)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("TLS collector listening on %s", l.Addr())
+
+	cs := NewServer(l, mc)
+	go cs.Start()
+
+	cc := &collectorT{t, NewTLSRemoteCollector(l.Addr().String(), &localhostTLSConfig)}
+	cc.MustCollect(SpanID{1, 2, 3})
+	cc.MustCollect(SpanID{2, 3, 4})
+	if err := cc.Collector.(*RemoteCollector).Close(); err != nil {
+		t.Error(err)
+	}
+
+	time.Sleep(20 * time.Millisecond)
+	if want := 2; numPackets != want {
+		t.Errorf("server collected %d packets, want %d", numPackets, want)
+	}
+}
+
+func TestChunkedCollector(t *testing.T) {
+	var packets []collectPacket
+	mc := &mockCollector{
+		Collect_: func(span SpanID, anns ...Annotation) error {
+			packets = append(packets, collectPacket{span, anns})
+			return nil
+		},
+	}
+
+	cc := &ChunkedCollector{
+		Collector:   mc,
+		MinInterval: time.Millisecond * 10,
+	}
+	cc.Collect(SpanID{1, 2, 3}, Annotation{"k1", []byte("v1")})
+	cc.Collect(SpanID{1, 2, 3}, Annotation{"k2", []byte("v2")})
+	cc.Collect(SpanID{2, 3, 4}, Annotation{"k3", []byte("v3")})
+	cc.Collect(SpanID{1, 2, 3}, Annotation{"k4", []byte("v4")})
+
+	// Check before the MinInterval has elapsed.
+	if len(packets) != 0 {
+		t.Errorf("before MinInterval: got len(packets) == %d, want 0", len(packets))
+	}
+
+	time.Sleep(cc.MinInterval * 2)
+
+	// Check after the MinInterval has elapsed.
+	want := []collectPacket{
+		{SpanID{1, 2, 3}, Annotations{{"k1", []byte("v1")}, {"k2", []byte("v2")}, {"k4", []byte("v4")}}},
+		{SpanID{2, 3, 4}, Annotations{{"k3", []byte("v3")}}},
+	}
+	if !reflect.DeepEqual(packets, want) {
+		t.Errorf("after MinInterval: got packets == %v, want %v", packets, want)
+	}
+
+	// Check that Stop stops it.
+	lenBeforeStop := len(packets)
+	cc.Stop()
+	cc.Collect(SpanID{1, 2, 3}, Annotation{"k5", []byte("v5")})
+	time.Sleep(cc.MinInterval * 2)
+	if len(packets) != lenBeforeStop {
+		t.Errorf("after Stop: got len(packets) == %d, want %d", len(packets), lenBeforeStop)
+	}
+}
+
 type mockCollector struct {
 	Collect_ func(SpanID, ...Annotation) error
 }
@@ -7,3 +202,59 @@ type mockCollector struct {
 func (c mockCollector) Collect(id SpanID, as ...Annotation) error {
 	return c.Collect_(id, as...)
 }
+
+type collectorT struct {
+	t *testing.T
+	Collector
+}
+
+func (s collectorT) MustCollect(id SpanID, as ...Annotation) {
+	if err := s.Collector.Collect(id, as...); err != nil {
+		s.t.Fatalf("Collect(%+v, %v): %s", id, as, err)
+	}
+}
+
+var localhostTLSConfig tls.Config
+
+func init() {
+	cert, err := tls.X509KeyPair(localhostCert, localhostKey)
+	if err != nil {
+		panic(fmt.Sprintf("localhostTLSConfig: %v", err))
+	}
+	localhostTLSConfig.Certificates = []tls.Certificate{cert}
+
+	certPool := x509.NewCertPool()
+	if ok := certPool.AppendCertsFromPEM(localhostCert); !ok {
+		panic("AppendCertsFromPEM: !ok")
+	}
+	localhostTLSConfig.RootCAs = certPool
+
+	localhostTLSConfig.BuildNameToCertificate()
+}
+
+// localhostCert is a PEM-encoded TLS cert with SAN IPs
+// "127.0.0.1" and "[::1]", expiring at the last second of 2049 (the end
+// of ASN.1 time).
+// generated from src/crypto/tls:
+// go run generate_cert.go  --rsa-bits 512 --host 127.0.0.1,::1,example.com --ca --start-date "Jan 1 00:00:00 1970" --duration=1000000h
+var localhostCert = []byte(`-----BEGIN CERTIFICATE-----
+MIIBdzCCASOgAwIBAgIBADALBgkqhkiG9w0BAQUwEjEQMA4GA1UEChMHQWNtZSBD
+bzAeFw03MDAxMDEwMDAwMDBaFw00OTEyMzEyMzU5NTlaMBIxEDAOBgNVBAoTB0Fj
+bWUgQ28wWjALBgkqhkiG9w0BAQEDSwAwSAJBAN55NcYKZeInyTuhcCwFMhDHCmwa
+IUSdtXdcbItRB/yfXGBhiex00IaLXQnSU+QZPRZWYqeTEbFSgihqi1PUDy8CAwEA
+AaNoMGYwDgYDVR0PAQH/BAQDAgCkMBMGA1UdJQQMMAoGCCsGAQUFBwMBMA8GA1Ud
+EwEB/wQFMAMBAf8wLgYDVR0RBCcwJYILZXhhbXBsZS5jb22HBH8AAAGHEAAAAAAA
+AAAAAAAAAAAAAAEwCwYJKoZIhvcNAQEFA0EAAoQn/ytgqpiLcZu9XKbCJsJcvkgk
+Se6AbGXgSlq+ZCEVo0qIwSgeBqmsJxUu7NCSOwVJLYNEBO2DtIxoYVk+MA==
+-----END CERTIFICATE-----`)
+
+// localhostKey is the private key for localhostCert.
+var localhostKey = []byte(`-----BEGIN RSA PRIVATE KEY-----
+MIIBPAIBAAJBAN55NcYKZeInyTuhcCwFMhDHCmwaIUSdtXdcbItRB/yfXGBhiex0
+0IaLXQnSU+QZPRZWYqeTEbFSgihqi1PUDy8CAwEAAQJBAQdUx66rfh8sYsgfdcvV
+NoafYpnEcB5s4m/vSVe6SU7dCK6eYec9f9wpT353ljhDUHq3EbmE4foNzJngh35d
+AekCIQDhRQG5Li0Wj8TM4obOnnXUXf1jRv0UkzE9AHWLG5q3AwIhAPzSjpYUDjVW
+MCUXgckTpKCuGwbJk7424Nb8bLzf3kllAiA5mUBgjfr/WtFSJdWcPQ4Zt9KTMNKD
+EUO0ukpTwEIl6wIhAMbGqZK3zAAFdq8DD2jPx+UJXnh0rnOkZBzDtJ6/iN69AiEA
+1Aq8MJgTaYsDQWyU/hDq5YkDJc9e9DSCvUIzqxQWMQE=
+-----END RSA PRIVATE KEY-----`)
