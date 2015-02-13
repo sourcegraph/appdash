344cb92340e4aad2c35cfd53aa692286f6de6519
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-02-13 16:40:24 -0700

    sqltrace: apptrace -> appdash

--- sqltrace/sql.go
+++ sqltrace/sql.go
@@ -4,10 +4,10 @@ package sqltrace
 import (
 	"time"
 
-	"sourcegraph.com/sourcegraph/apptrace"
+	"sourcegraph.com/sourcegraph/appdash"
 )
 
-// SQLEvent is an SQL query event for use with apptrace. It's primary function
+// SQLEvent is an SQL query event for use with appdash. It's primary function
 // is to measure the time between when the query is sent and later received.
 type SQLEvent struct {
 	SQL        string
@@ -16,16 +16,16 @@ type SQLEvent struct {
 	ClientRecv time.Time
 }
 
-// Schema implements the apptrace Event interface by returning this event's
+// Schema implements the appdash Event interface by returning this event's
 // constant schema string, "SQL".
 func (SQLEvent) Schema() string { return "SQL" }
 
-// Start implements the apptrace TimespanEvent interface by returning the time
+// Start implements the appdash TimespanEvent interface by returning the time
 // at which the SQL query was sent out.
 func (e SQLEvent) Start() time.Time { return e.ClientSend }
 
-// End implements the apptrace TimespanEvent interface by returning the time at
+// End implements the appdash TimespanEvent interface by returning the time at
 // which the SQL query returned / was received.
 func (e SQLEvent) End() time.Time { return e.ClientRecv }
 
-func init() { apptrace.RegisterEvent(SQLEvent{}) }
+func init() { appdash.RegisterEvent(SQLEvent{}) }
