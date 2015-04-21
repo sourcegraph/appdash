dc658aa5f09e38733451a133d579561f06576fe2
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-04-21 13:02:34 -0700

    Do not load template data from disk by default.
    This change replaces the previously-used Makefile with go:generate
    directives (for which go:generate was designed). The make targets `gen-dist` and
    `gen-dev` are kept and produce a warning message for backwards compatibility. It
    also alters the default such that building an Appdash app will always produce a
    binary that includes the template data.
    
    This is easier for normal users, as majority of the time they are not interested
    in modifying the template data. Below is a translation of the make commands to
    their new equivalents:
    
      $ go generate
    
    Will produce two files containing the template data. By default, the data will
    be built into the binary for easy deployment etc by users.
    
      $ go install -tags=debug ./...
    
    Building the code with the debug build tag will enable usage of the second Go
    file generated, which loads the template data from disk (useful for development
    of the template files).
    
    Fixes #59

--- traceapp/tmpl/data_src.go
+++ traceapp/tmpl/data_src.go
@@ -1,29 +1,269 @@
-// +build !dist
+// +build debug
 
 package tmpl
 
 import (
-	"go/build"
-	"log"
+	"fmt"
+	"io/ioutil"
 	"os"
+	"path"
 	"path/filepath"
+	"strings"
 )
 
-// rootDir is the directory containing the html/template template files.
-var rootDir = filepath.Join(defaultBase("sourcegraph.com/sourcegraph/appdash/traceapp"), "tmpl")
+// bindata_read reads the given file from disk. It returns an error on failure.
+func bindata_read(path, name string) ([]byte, error) {
+	buf, err := ioutil.ReadFile(path)
+	if err != nil {
+		err = fmt.Errorf("Error reading asset %s at %s: %v", name, path, err)
+	}
+	return buf, err
+}
 
-func defaultBase(path string) string {
-	p, err := build.Default.Import(path, "", build.FindOnly)
+type asset struct {
+	bytes []byte
+	info  os.FileInfo
+}
+
+// aggregate_html reads file data from disk. It returns an error on failure.
+func aggregate_html() (*asset, error) {
+	path := "/home/stephen/Desktop/godev/src/sourcegraph.com/sourcegraph/appdash/traceapp/tmpl/aggregate.html"
+	name := "aggregate.html"
+	bytes, err := bindata_read(path, name)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	cwd, err := os.Getwd()
+
+	fi, err := os.Stat(path)
 	if err != nil {
-		log.Fatal(err)
+		err = fmt.Errorf("Error reading asset info %s at %s: %v", name, path, err)
 	}
-	p.Dir, err = filepath.Rel(cwd, p.Dir)
+
+	a := &asset{bytes: bytes, info: fi}
+	return a, err
+}
+
+// layout_html reads file data from disk. It returns an error on failure.
+func layout_html() (*asset, error) {
+	path := "/home/stephen/Desktop/godev/src/sourcegraph.com/sourcegraph/appdash/traceapp/tmpl/layout.html"
+	name := "layout.html"
+	bytes, err := bindata_read(path, name)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	return p.Dir
+
+	fi, err := os.Stat(path)
+	if err != nil {
+		err = fmt.Errorf("Error reading asset info %s at %s: %v", name, path, err)
+	}
+
+	a := &asset{bytes: bytes, info: fi}
+	return a, err
+}
+
+// root_html reads file data from disk. It returns an error on failure.
+func root_html() (*asset, error) {
+	path := "/home/stephen/Desktop/godev/src/sourcegraph.com/sourcegraph/appdash/traceapp/tmpl/root.html"
+	name := "root.html"
+	bytes, err := bindata_read(path, name)
+	if err != nil {
+		return nil, err
+	}
+
+	fi, err := os.Stat(path)
+	if err != nil {
+		err = fmt.Errorf("Error reading asset info %s at %s: %v", name, path, err)
+	}
+
+	a := &asset{bytes: bytes, info: fi}
+	return a, err
+}
+
+// trace_html reads file data from disk. It returns an error on failure.
+func trace_html() (*asset, error) {
+	path := "/home/stephen/Desktop/godev/src/sourcegraph.com/sourcegraph/appdash/traceapp/tmpl/trace.html"
+	name := "trace.html"
+	bytes, err := bindata_read(path, name)
+	if err != nil {
+		return nil, err
+	}
+
+	fi, err := os.Stat(path)
+	if err != nil {
+		err = fmt.Errorf("Error reading asset info %s at %s: %v", name, path, err)
+	}
+
+	a := &asset{bytes: bytes, info: fi}
+	return a, err
+}
+
+// traces_html reads file data from disk. It returns an error on failure.
+func traces_html() (*asset, error) {
+	path := "/home/stephen/Desktop/godev/src/sourcegraph.com/sourcegraph/appdash/traceapp/tmpl/traces.html"
+	name := "traces.html"
+	bytes, err := bindata_read(path, name)
+	if err != nil {
+		return nil, err
+	}
+
+	fi, err := os.Stat(path)
+	if err != nil {
+		err = fmt.Errorf("Error reading asset info %s at %s: %v", name, path, err)
+	}
+
+	a := &asset{bytes: bytes, info: fi}
+	return a, err
+}
+
+// Asset loads and returns the asset for the given name.
+// It returns an error if the asset could not be found or
+// could not be loaded.
+func Asset(name string) ([]byte, error) {
+	cannonicalName := strings.Replace(name, "\\", "/", -1)
+	if f, ok := _bindata[cannonicalName]; ok {
+		a, err := f()
+		if err != nil {
+			return nil, fmt.Errorf("Asset %s can't read by error: %v", name, err)
+		}
+		return a.bytes, nil
+	}
+	return nil, fmt.Errorf("Asset %s not found", name)
+}
+
+// MustAsset is like Asset but panics when Asset would return an error.
+// It simplifies safe initialization of global variables.
+func MustAsset(name string) []byte {
+	a, err := Asset(name)
+	if err != nil {
+		panic("asset: Asset(" + name + "): " + err.Error())
+	}
+
+	return a
+}
+
+// AssetInfo loads and returns the asset info for the given name.
+// It returns an error if the asset could not be found or
+// could not be loaded.
+func AssetInfo(name string) (os.FileInfo, error) {
+	cannonicalName := strings.Replace(name, "\\", "/", -1)
+	if f, ok := _bindata[cannonicalName]; ok {
+		a, err := f()
+		if err != nil {
+			return nil, fmt.Errorf("AssetInfo %s can't read by error: %v", name, err)
+		}
+		return a.info, nil
+	}
+	return nil, fmt.Errorf("AssetInfo %s not found", name)
+}
+
+// AssetNames returns the names of the assets.
+func AssetNames() []string {
+	names := make([]string, 0, len(_bindata))
+	for name := range _bindata {
+		names = append(names, name)
+	}
+	return names
+}
+
+// _bindata is a table, holding each asset generator, mapped to its name.
+var _bindata = map[string]func() (*asset, error){
+	"aggregate.html": aggregate_html,
+	"layout.html":    layout_html,
+	"root.html":      root_html,
+	"trace.html":     trace_html,
+	"traces.html":    traces_html,
+}
+
+// AssetDir returns the file names below a certain
+// directory embedded in the file by go-bindata.
+// For example if you run go-bindata on data/... and data contains the
+// following hierarchy:
+//     data/
+//       foo.txt
+//       img/
+//         a.png
+//         b.png
+// then AssetDir("data") would return []string{"foo.txt", "img"}
+// AssetDir("data/img") would return []string{"a.png", "b.png"}
+// AssetDir("foo.txt") and AssetDir("notexist") would return an error
+// AssetDir("") will return []string{"data"}.
+func AssetDir(name string) ([]string, error) {
+	node := _bintree
+	if len(name) != 0 {
+		cannonicalName := strings.Replace(name, "\\", "/", -1)
+		pathList := strings.Split(cannonicalName, "/")
+		for _, p := range pathList {
+			node = node.Children[p]
+			if node == nil {
+				return nil, fmt.Errorf("Asset %s not found", name)
+			}
+		}
+	}
+	if node.Func != nil {
+		return nil, fmt.Errorf("Asset %s not found", name)
+	}
+	rv := make([]string, 0, len(node.Children))
+	for name := range node.Children {
+		rv = append(rv, name)
+	}
+	return rv, nil
+}
+
+type _bintree_t struct {
+	Func     func() (*asset, error)
+	Children map[string]*_bintree_t
+}
+
+var _bintree = &_bintree_t{nil, map[string]*_bintree_t{
+	"aggregate.html": &_bintree_t{aggregate_html, map[string]*_bintree_t{}},
+	"layout.html":    &_bintree_t{layout_html, map[string]*_bintree_t{}},
+	"root.html":      &_bintree_t{root_html, map[string]*_bintree_t{}},
+	"trace.html":     &_bintree_t{trace_html, map[string]*_bintree_t{}},
+	"traces.html":    &_bintree_t{traces_html, map[string]*_bintree_t{}},
+}}
+
+// Restore an asset under the given directory
+func RestoreAsset(dir, name string) error {
+	data, err := Asset(name)
+	if err != nil {
+		return err
+	}
+	info, err := AssetInfo(name)
+	if err != nil {
+		return err
+	}
+	err = os.MkdirAll(_filePath(dir, path.Dir(name)), os.FileMode(0755))
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(_filePath(dir, name), data, info.Mode())
+	if err != nil {
+		return err
+	}
+	err = os.Chtimes(_filePath(dir, name), info.ModTime(), info.ModTime())
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Restore assets under the given directory recursively
+func RestoreAssets(dir, name string) error {
+	children, err := AssetDir(name)
+	if err != nil { // File
+		return RestoreAsset(dir, name)
+	} else { // Dir
+		for _, child := range children {
+			err = RestoreAssets(dir, path.Join(name, child))
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func _filePath(dir, name string) string {
+	cannonicalName := strings.Replace(name, "\\", "/", -1)
+	return filepath.Join(append([]string{dir}, strings.Split(cannonicalName, "/")...)...)
 }
