94a4baa42b1b19fbac2abc3ed931987f71d0acd7
Dmitri Shuralyov <shurcooL@gmail.com>
2015-09-21 16:18:26 -0700

    Remove httpfs/filter since it's no longer needed.
    The new data subdirectory contains only the template data, nothing
    extra.

--- traceapp/tmpl/data.go
+++ traceapp/tmpl/data.go
@@ -2,18 +2,7 @@
 
 package tmpl
 
-import (
-	"net/http"
-	"os"
-	pathpkg "path"
-
-	"github.com/shurcooL/go/vfs/httpfs/filter"
-)
+import "net/http"
 
 // Data is a virtual filesystem that contains template data used by Appdash.
-var Data = filter.NewIgnore(
-	http.Dir(rootDir),
-	func(fi os.FileInfo, _ string) bool {
-		return !fi.IsDir() && pathpkg.Ext(fi.Name()) == ".go"
-	},
-)
+var Data = http.Dir(rootDir)
